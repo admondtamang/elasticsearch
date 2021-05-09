5bccd98265ab851275845a42dc934f5cfcfd49db
Admond Tamang <admondtamang@gmail.com>
2021-05-09 14:50:51 +0545

    [add] docker concontainer

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const morgan = require("morgan");
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(morgan("tiny"));
+
+app.get("/", (req, res) => {
+    res.send("<h2>hello2!!</h2>");
+});
 
 app.use("/api", require("./routes/create"));
 
